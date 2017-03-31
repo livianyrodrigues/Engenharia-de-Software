0bdc8bb28ace1caf1b418c9eb83a6c26a607c9ae
Livian y Reis <livianyrodrigues@hotmail.com>
2017-03-31 15:01:40 -0300

    Adicionando novos atributos

--- Brenner/src/Inicial/ES20162.java
+++ Brenner/src/Inicial/ES20162.java
@@ -1,6 +1,13 @@
 package Inicial;
 
 public class ES20162 {
+	
+	public String nome;
+	public int idade;
+	
+	public ES20162(){
+	
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
